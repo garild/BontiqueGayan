9c4d11b2bc5288e0e4aeeab7554e46089d8f9e47
Garib Tigranyan <garibo@o2.pl>
2020-06-04 16:02:14 +0200

    code refactor

--- src/BoutiqueShopp/Common/Helper/GlobalApplicationSettings.cs
+++ src/BoutiqueShopp/Common/Helper/GlobalApplicationSettings.cs
@@ -1,14 +1,17 @@
-﻿namespace SkyReg
+﻿using Database;
+using SkyReg.Extensions;
+using System;
+
+namespace SkyReg
 {
     public sealed class GlobalApplicationSettings
     {
-        public static int UserId { get; set; }
-        public static string UserLogin { get; set; }
         public static string DatabaseConfigFile { get; set; }
         public static string UserConfigFile { get; set; }
         public static string GlobalPathFile { get; set; }
         public static string AppVer { get; set; }
-        public static string LocalMachineName { get; set; }
+        public static string LocalMachineName { get; set; } = Environment.CurrentDirectory;
         public static bool IsDbExists { get; set; }
+        public static LoggedUser User  { get; set; }
     }
 }
---
+++ src/BoutiqueShopp/Common/Helper/LoggedUser.cs
@@ -0,0 +1,8 @@
+﻿namespace SkyReg.Extensions
+{
+    public class LoggedUser
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+    }
+}

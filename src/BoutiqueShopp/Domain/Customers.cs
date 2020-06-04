29b10203b71f2c88fe698671b7e07484b824b7a7
Garib Tigranyan <garibo@o2.pl>
2020-06-04 15:49:20 +0200

    added new aggreagt Customers and interface

---
+++ src/BoutiqueShopp/Domain/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoutiqueShopp.Domain
+{
+    public class Customers
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Nick { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
---
+++ src/BoutiqueShopp/Domain/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BoutiqueShopp.Domain
+{
+    public interface ICustomerRepository
+    {
+        Customers FindBy(string name, string LastName);
+        List<Customers> GetAll();
+        void Update(Customers customers);
+        void Delete(Customers customers);
+    }
+}
